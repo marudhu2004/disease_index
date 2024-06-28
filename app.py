1f25aa812e104c849b2255a10541612dcebfc1bb
Marudhu Paandian.K <46159834+marudhu2004@users.noreply.github.com>
2024-06-28 09:13:20 +0530

    updated app to have multiple tables

--- app.py
+++ app.py
@@ -20,9 +20,13 @@ def index():
 def disease(disease_id):
     conn = get_db_connection()
     disease = conn.execute('SELECT * FROM disease WHERE id = ?', (disease_id,)).fetchone()
-    info = conn.execute('SELECT * FROM disease_info WHERE disease_id = ?', (disease_id,)).fetchone()
+    causes = conn.execute('SELECT * FROM causes WHERE disease_id = ?', (disease_id,)).fetchall()
+    symptoms = conn.execute('SELECT * FROM symptoms WHERE disease_id = ?', (disease_id,)).fetchall()
+    prevention = conn.execute('SELECT * FROM prevention WHERE disease_id = ?', (disease_id,)).fetchall()
+    cure = conn.execute('SELECT * FROM cure WHERE disease_id = ?', (disease_id,)).fetchall()
+    specialists = conn.execute('SELECT * FROM specialists WHERE disease_id = ?', (disease_id,)).fetchall()
     conn.close()
-    return render_template('disease.html', disease=disease, info=info)
+    return render_template('disease.html', disease=disease, causes=causes, symptoms=symptoms, prevention=prevention, cure=cure, specialists=specialists)
 
 @app.route('/add_disease', methods=('GET', 'POST'))
 def add_disease():
@@ -40,26 +44,29 @@ def add_disease():
 
 @app.route('/add_info/<int:disease_id>', methods=('GET', 'POST'))
 def add_info(disease_id):
-    conn = get_db_connection()
-    disease = conn.execute('SELECT * FROM disease WHERE id = ?', (disease_id,)).fetchone()
-    conn.close()
-
     if request.method == 'POST':
-        causes = request.form['causes']
-        symptoms = request.form['symptoms']
-        prevention = request.form['prevention']
-        cure = request.form['cure']
-        specialists = request.form['specialists']
+        causes = request.form.getlist('causes')
+        symptoms = request.form.getlist('symptoms')
+        prevention = request.form.getlist('prevention')
+        cure = request.form.getlist('cure')
+        specialists = request.form.getlist('specialists')
 
         conn = get_db_connection()
-        conn.execute('INSERT INTO disease_info (disease_id, causes, symptoms, prevention, cure, specialists) VALUES (?, ?, ?, ?, ?, ?)', 
-                     (disease_id, causes, symptoms, prevention, cure, specialists))
+        for cause in causes:
+            conn.execute('INSERT INTO causes (disease_id, cause) VALUES (?, ?)', (disease_id, cause))
+        for symptom in symptoms:
+            conn.execute('INSERT INTO symptoms (disease_id, symptom) VALUES (?, ?)', (disease_id, symptom))
+        for prevent in prevention:
+            conn.execute('INSERT INTO prevention (disease_id, prevention) VALUES (?, ?)', (disease_id, prevent))
+        for c in cure:
+            conn.execute('INSERT INTO cure (disease_id, cure) VALUES (?, ?)', (disease_id, c))
+        for specialist in specialists:
+            conn.execute('INSERT INTO specialists (disease_id, specialist) VALUES (?, ?)', (disease_id, specialist))
         conn.commit()
         conn.close()
         return redirect(url_for('disease', disease_id=disease_id))
 
-    return render_template('add_info.html', disease_id=disease_id, disease=disease)
-
+    return render_template('add_info.html', disease_id=disease_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
