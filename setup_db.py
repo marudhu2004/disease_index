2cdaee11dd54a605c4fcaab5224dda1bbd8a1f2f
Marudhu Paandian.K <46159834+marudhu2004@users.noreply.github.com>
2024-06-28 09:13:07 +0530

    updated table structure

--- setup_db.py
+++ setup_db.py
@@ -11,13 +11,38 @@ c.execute('''CREATE TABLE IF NOT EXISTS disease (
                 category TEXT NOT NULL
             )''')
 
-c.execute('''CREATE TABLE IF NOT EXISTS disease_info (
+c.execute('''CREATE TABLE IF NOT EXISTS causes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 disease_id INTEGER,
-                causes TEXT,
-                symptoms TEXT,
+                cause TEXT,
+                FOREIGN KEY (disease_id) REFERENCES disease (id)
+            )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS symptoms (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                disease_id INTEGER,
+                symptom TEXT,
+                FOREIGN KEY (disease_id) REFERENCES disease (id)
+            )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS prevention (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                disease_id INTEGER,
                 prevention TEXT,
+                FOREIGN KEY (disease_id) REFERENCES disease (id)
+            )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS cure (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                disease_id INTEGER,
                 cure TEXT,
-                specialists TEXT,
+                FOREIGN KEY (disease_id) REFERENCES disease (id)
+            )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS specialists (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                disease_id INTEGER,
+                specialist TEXT,
                 FOREIGN KEY (disease_id) REFERENCES disease (id)
             )''')
 
